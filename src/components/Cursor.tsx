0f04c42c7db273eefc49fa30df0e37032ccfe0fd
shady.xlyres <157346373+shadyxlyres@users.noreply.github.com>
2025-01-30 16:39:25 +0530

    Update Cursor.tsx

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -1,28 +1,33 @@
 import { useEffect, useRef } from "react";
 import "./styles/Cursor.css";
-import gsap from "gsap";
 
 const Cursor = () => {
   const cursorRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     let hover = false;
     const cursor = cursorRef.current!;
     const mousePos = { x: 0, y: 0 };
     const cursorPos = { x: 0, y: 0 };
+    
     document.addEventListener("mousemove", (e) => {
       mousePos.x = e.clientX;
       mousePos.y = e.clientY;
     });
-    requestAnimationFrame(function loop() {
+
+    const loop = () => {
       if (!hover) {
         const delay = 6;
         cursorPos.x += (mousePos.x - cursorPos.x) / delay;
         cursorPos.y += (mousePos.y - cursorPos.y) / delay;
-        gsap.to(cursor, { x: cursorPos.x, y: cursorPos.y, duration: 0.1 });
-        // cursor.style.transform = `translate(${cursorPos.x}px, ${cursorPos.y}px)`;
+        
+        cursor.style.transform = `translate(${cursorPos.x}px, ${cursorPos.y}px)`;
       }
       requestAnimationFrame(loop);
-    });
+    };
+    
+    requestAnimationFrame(loop);
+
     document.querySelectorAll("[data-cursor]").forEach((item) => {
       const element = item as HTMLElement;
       element.addEventListener("mouseover", (e: MouseEvent) => {
@@ -31,9 +36,7 @@ const Cursor = () => {
 
         if (element.dataset.cursor === "icons") {
           cursor.classList.add("cursor-icons");
-
-          gsap.to(cursor, { x: rect.left, y: rect.top, duration: 0.1 });
-          //   cursor.style.transform = `translate(${rect.left}px,${rect.top}px)`;
+          cursor.style.transform = `translate(${rect.left}px, ${rect.top}px)`;
           cursor.style.setProperty("--cursorH", `${rect.height}px`);
           hover = true;
         }
@@ -41,11 +44,21 @@ const Cursor = () => {
           cursor.classList.add("cursor-disable");
         }
       });
+      
       element.addEventListener("mouseout", () => {
         cursor.classList.remove("cursor-disable", "cursor-icons");
         hover = false;
       });
     });
+    
+    return () => {
+      document.removeEventListener("mousemove", () => {});
+      document.querySelectorAll("[data-cursor]").forEach((item) => {
+        const element = item as HTMLElement;
+        element.removeEventListener("mouseover", () => {});
+        element.removeEventListener("mouseout", () => {});
+      });
+    };
   }, []);
 
   return <div className="cursor-main" ref={cursorRef}></div>;
