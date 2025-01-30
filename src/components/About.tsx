a92ae6b2e575d120e0f1ed3ced8b4edb1df79dd2
shady.xlyres <157346373+shadyxlyres@users.noreply.github.com>
2025-01-30 10:48:15 +0530

    Add files via upload

---
+++ src/components/About.tsx
@@ -0,0 +1,16 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+        Full Stack Developer: A versatile tech professional skilled in both front-end and back-end development. Proficient in languages like JavaScript, Python, and frameworks like React and Node.js, they create dynamic web applications, ensuring seamless user experiences and robust server-side functionality.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
